73934d80a155aa0f0da6f48c300e236f7947490f
domin <dominika.mmatyka@gmail.com>
2024-06-21 09:57:53 +0200

    Initialize repo

---
+++ src/main/java/com/stockmanagement/SuppliersOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.stockmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppliersOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuppliersOrdersApplication.class, args);
+	}
+
+}
