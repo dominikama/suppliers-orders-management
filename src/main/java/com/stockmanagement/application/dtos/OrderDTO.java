590d8dcaf3ea1496cbb5d0a370fed726c69bb315
domin <dominika.mmatyka@gmail.com>
2024-06-22 10:47:02 +0200

    create dtos

---
+++ src/main/java/com/stockmanagement/application/dtos/OrderDTO.java
@@ -0,0 +1,84 @@
+package com.stockmanagement.application.dtos;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class OrderDTO {
+
+    private Long id;
+    private LocalDate orderDate;
+    private Long supplierId;
+    private Long warehouseId;
+    private List<ProductOrderDTO> products;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Long getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(Long supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public Long getWarehouseId() {
+        return warehouseId;
+    }
+
+    public void setWarehouseId(Long warehouseId) {
+        this.warehouseId = warehouseId;
+    }
+
+    public List<ProductOrderDTO> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<ProductOrderDTO> products) {
+        this.products = products;
+    }
+
+    public static class ProductOrderDTO {
+        private Long productId;
+        private String productCode;
+        private int quantity;
+
+        public Long getProductId() {
+            return productId;
+        }
+
+        public void setProductId(Long productId) {
+            this.productId = productId;
+        }
+
+        public String getProductCode() {
+            return productCode;
+        }
+
+        public void setProductCode(String productCode) {
+            this.productCode = productCode;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+    }
+}
+
